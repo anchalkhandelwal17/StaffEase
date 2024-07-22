18f3eb31263edb8e0c1c74a0cc7f989c9049da1a
Anchal khandelwal <Anchalkhandelwal999@gmail.com>
2024-07-22 21:45:23 +0530

    commit

---
+++ src/main/java/com/springboot/crud/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.springboot.crud.service;
+
+import com.springboot.crud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
